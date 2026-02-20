d375ad62e21ece4bb261c81490285485348d277b
Shishir Neupane <shishirapk@gmail.com>
2026-02-20 18:44:16 +0545

    Fix: Pass full cloudinary namespace (with .v2) to CloudinaryStorage

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,11 +1,11 @@
-import { v2 as cloudinary } from 'cloudinary';
+import * as cloudinary from 'cloudinary';
 import CloudinaryStorage from 'multer-storage-cloudinary';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 // Configure Cloudinary
-cloudinary.config({
+cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
@@ -14,6 +14,7 @@ cloudinary.config({
 console.log("Cloudinary config:", { cloud_name: process.env.CLOUDINARY_CLOUD_NAME });
 
 // Configure Multer Storage - CloudinaryStorage is the default export
+// Note: CloudinaryStorage expects the full cloudinary namespace with .v2 property
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
